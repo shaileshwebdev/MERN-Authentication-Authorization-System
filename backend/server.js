a13728bda024d946bd68b5bde67b8bdbd3b933a3
shaileshwebdev <shaileshsahani001@gmail.com>
2025-08-28 12:19:54 +0530

    cors error fix

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,23 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:5173", // local dev
+  "https://mern-authentication-authorization-s.vercel.app/", // vercel frontend
+];
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps, curl, postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // if using cookies/auth headers
   })
 );
 
