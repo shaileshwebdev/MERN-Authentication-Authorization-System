90c4afc027d2176a78d7018719636a5b06f90bfd
shaileshwebdev <shaileshsahani001@gmail.com>
2025-08-28 13:00:07 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://mern-authentication-authorization-system-g37a432cf.vercel.app",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // <-- add this
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
