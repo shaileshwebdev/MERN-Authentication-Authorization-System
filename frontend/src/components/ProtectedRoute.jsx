aca1aadb79165831c70631df87f859bffd392133
shaileshwebdev <138277595+shaileshwebdev@users.noreply.github.com>
2025-08-29 00:17:25 +0530

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const { user } = getData();
-  return <div>{user ? children : <Navigate to={"/login"} />}</div>;
+  return <div>{user ? children : <Navigate to={"/signup"} />}</div>;
 };
 
 export default ProtectedRoute;
