6dc8d499331612298e874516d6131fd58f08b419
shaileshwebdev <shaileshsahani001@gmail.com>
2025-08-28 12:01:58 +0530

    fix component error

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import Hero from "@/components/hero";
-import Navbar from "@/components/navbar";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 const Home = () => {
